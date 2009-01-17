029b1c770a50c7d5aec3683a8e94eb0690a97388
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-01-17 19:41:16 +0000

    Add Horde guide.

--- Horde.lua
+++ Horde.lua
@@ -1,7 +1,150 @@
 
-TourGuide:RegisterGuide("The Lunar Festival", nil, "Horde", function() return [[
+TourGuide:RegisterGuide("The Lunar Festival (80+)", nil, "Horde", function() return [[
 
+N Wall of Text |N|This guide assumes you are level 80, and/or are in a large group. You will be visiting some nasty places which may be hard to solo. Also, please make sure you start with zero [Coin of Ancestry] in your bags, follow this guide exactly and don't skip unless told to, otherwise it won't automatically update (there's no guarantee it will anyway, but you can still tick them off manually). When you are ready to begin, skip this message. Good luck, brave adventurer!|
 
+h Dalaran |N|If you are currently in Dalaran or Shattrath, make sure to set your Hearthstone there if you haven't already done so. You'll be coming back later.|
+R Orgrimmar |N|Use the portal|
+
+F Azshara
+N Skygleam the Elder |N|Ravencrest Monument, south-east (72,85) |L|21100 1|
+
+F Everlook
+N Stonespire the Elder |N|In the center of Everlook (61,37)| |L|21100 2|
+N Brightspear the Elder |N|Southeastern edge of Lake Kel'Theril (55,43) |L|21100 3|
+
+F Bloodvenom Post
+N Nightwind the Elder |N|North of Jaedenar (37,52)| |L|21100 4|
+
+F Emerald Sanctuary
+R Astranaar
+N Riversong the Elder |N|In the gazebo near the Hippogryph Master (35,49) |L|21100 5|
+
+F Darkshore
+N Starweave the Elder |N|Near the Hippogryph Master (36,46)| |L|21100 6|
+
+b Darnassus
+N Bladeswift the Elder |N|Center of the Cenarion Enclave (33,14)| |L|21100 7|
+N Bladeleaf the Elder |N|Southern edge of Dolanaar (57,60)| |L|21100 8|
+
+R Orgrimmar |N|Hearth to Shattrath/Dalaran, then take the portal|
+N Darkhorn the Elder |N|Outside Thrall's chamber, Valley of Wisdom (41,33)| |L|21100 9|
+
+R Razor Hill
+N Runetotem the Elder |N|SE edge of Razor Hill (54,44)| |L|21100 10|
+
+R Ratchet
+N Windtotem the Elder |N|In Ratchet, outside of the Engineering building north of the bank (62,37)| |L|21100 11|
+
+F Crossroads
+N Moonwarden the Elder |N|Next to the flight master in the Crossroads (51,28)| |L|21100 12|
+
+F Camp Taurajo
+N High Mountain the Elder |N|NE edge of Camp Taurajo, next to the forge (45,58)| |L|21100 13|
+
+R Mulgore
+N Bloodhoof the Elder |N|North of Bloodhoof Village, next to the bridge (48,53)| |L|21100 14|
+
+R Thunder Bluff
+N Proudhorn the Elder |N|Elder Rise (72,23)| |L|21100 15|
+
+F Shadowprey Village
+R Maraudon
+N Splitrock the Elder |N|Jump into the water by the Princess and Rotgrip. Swim to the east and head up the ramp. Turn right and head up a 2nd ramp. The Elder will be in front of you.| |L|21100 16|
+
+F Camp Mojache
+N Grimtotem the Elder |N|Lariss Pavilion, northeast of Camp Mojache (76,37)| |L|21100 17|
+N Mistwalker the Elder |N|Inside the Dire Maul arena, outside the instance (62,31)| |L|21100 18|
+
+F Freewind Post
+N Skyseer the Elder |N|Middle of Freewind Post (45,50)| |L|21100 19|
+N Morningdew the Elder |N|Mirage Raceway, near the starting line (79,77)| |L|21100 20|
+
+R Tanaris
+N Dreamseer the Elder |N|In Gadgetzan (50,28)| |L|21100 21|
+
+R Zul'Farrak
+N Wildmane the Elder |N|SE corner of the room with Gahz'rilla's gong| |L|21100 22|
+
+R Tanaris
+N Ragetotem the Elder |N|Valley of the Watchers, west side (36,80) |L|21100 23|
+
+R Un'Goro Crater
+N Thunderhorn the Elder |N|North of the Slithering Scar (51,75)| |L|21100 24|
+
+R Silithus
+N Primestone the Elder |N|Center of the Crystal Vale (23,12)| |L|21100 25|
+N Bladesing the Elder |N|Standing in the Moon Well, Cenarion Hold (50,36)| |L|21100 26|
+
+R Undercity |N|Hearth to Shattrath/Dalaran, then take the portal|
+N Darkcore the Elder |N|In the middle of Undercity, above ground (67,38)| |L|21100 27|
+
+F Light's Hope Chapel
+N Snowcrown the Elder |N|Light's Hope Chapel (81,60)| |L|21100 28|
+N Windrun the Elder |N|Crown Guard Tower, north of Darrowshire (39,75)| |L|21100 29|
+
+R Stratholme
+N Farwhisper the Elder |N|North of the Festival Lane mailbox (near The Unforgiven). Enter through the back door. Make your first left in the instance, go through the gate, turn right, and the Elder will be standing at the back.| |L|21100 30|
+
+R Western Plaguelands
+N Meadowrun the Elder |N|North of the road, south of The Weeping Cave (66,47)| |L|21100 31|
+N Moonstrike the Elder |N|On top of the Scholomance Tower, outside of the instance (69,74)| |L|21100 32|
+
+R Tirisfal Glades
+N Graveborn the Elder |N|Just south of Brill (61,53)| |L|21100 33|
+
+F The Sepulcher
+N Obsidian the Elder |N|Near to The Sepulcher (45,41)| |L|21100 34|
+
+F Revantusk Village
+N Highpeak the Elder |N|Near the Creeping Ruin (49,48)| |L|21100 35|
+
+F Kargath
+R Loch Modan
+N Silvervein the Elder |N|(33,46)| |L|21100 36|
+
+R Dun Morogh
+N Goldwell the Elder |N|Near the inn (46,51)| |L|21100 37|
+
+R City of Ironforge
+N Bronzebeard the Elder |N|In the Mystic Ward (29,16) |L|21100 38|
+
+R Stormwind City |N|Use the Deeprun Tram if you can|
+N Hammershout the Elder |N|Standing in the Moon Well, Park District| |L|21100 39|
+
+R Elwynn Forest
+N Stormbrow the Elder |N|Near the pond (40,63)| |L|21100 40|
+
+R Westfall
+N Skychaser the Elder |N|On top of the tower, north-east of Sentinel Hill (56,47)| |L|21100 41|
+
+R Stranglethorn Vale
+N Starglade the Elder |N|Near Zul'Gurub, in the right "courtyard" in front of the steps up to instance entrance (53.18)| |L|21100 42|
+
+F Booty Bay |N|Or just run there|
+N Winterhoof the Elder |N|Near Horde flight master on top of the bank (26,76)| |L|21100 43|
+
+F Stonard
+R Temple of Atal'Hakkar
+N Starsong the Elder |N|First left up spiral stairs, through dragonkin at top landing, stay left into main room with dragonkin| |L|21100 44|
+
+R Blasted Lands
+N Bellowrage the Elder |N|North of the Dark Portal (58,55)| |L|21100 45|
+
+F Flame Crest
+N Dawnstrider the Elder |N|Up at Flame Crest, that cave with the Libram guy (64,24)| |L|21100 46|
+N Rumblerock the Elder |N|On top of Dreadmaul Rock, north of Morgan's Vigil (82,46)| |L|21100 47|
+
+F Thorium Point
+N Ironband the Elder |N|Southwest corner in Blackchar Cave (21,79)| |L|21100 48|
+
+R Lower Blackrock Spire
+N Stonefort the Elder |N|At the first of the rope bridges| |L|21100 49|
+
+R Blackrock Depths
+N Morndeep the Elder |N|Inside the Ring of Law| |L|21100 50|
+
+N To Be Continued... |N|There are Elders in Northrend aswell, but we don't know where they are. Please feel free to contribute data!|
 
 ]] end)
 
