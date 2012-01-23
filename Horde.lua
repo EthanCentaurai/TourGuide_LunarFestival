51fe8366b56268a600d8ab97bd7823b510a22225
Ethan Centaurai <stevenblanchard86@gmail.com>
2012-01-23 10:44:14 +0000

    Update the Horde guide. It's not perfect, but all the new data should be there.

--- Horde.lua
+++ Horde.lua
@@ -2,94 +2,99 @@
 TourGuide:RegisterGuide("The Lunar Festival (77-85)", nil, "Horde", function() return [[
 
 h Orgrimmar |N|Trust me, you'll be coming back. A lot.|
-
 T Darkhorn the Elder |N|Near the flight master. (52.4,60)| |Z|Orgrimmar|
 
 A The Lunar Festival |N|From the Lunar Festival Emissary in Orgrimmar. Skip this and the follow-up if you already have a [Lunar Festival Invitation]| |QID|8873|
 T The Lunar Festival |QID|8873|
 
 A Lunar Fireworks |QID|8867|
-C Lunar Fireworks |N|Buy fireworks from the nearby vendor, launch them into the sky!| |QID|8867|
+C Lunar Fireworks |N|Buy fireworks from the nearby vendor and use them near a firework launcher.| |QID|8867|
 T Lunar Fireworks |QID|8867|
 
 A Valadar Starsong |QID|8883|
 
-R Moonglade |N|Stand in the big beam of light and use your Lunar Invitation, or use your Teleport spell if you are a Druid.| |U|21711|
+F Moonglade |N|Stand in the big beam of light and use your Lunar Invitation, or use your Teleport spell if you are a Druid.| |U|21711|
 T Valadar Starsong |N|(54,35)| |QID|8883| |Z|Moonglade|
 
-N Omen |N|You now have the option of summoning and defeating Omen. It's fairly simple to do; gather a group of players, launch 20 Green Rocket Clusters at Stormrage Barrow Den (65,65) and defeat the large two-headed puppy. Avoid his Starfall attacks if you can. Once he's dead, stand in the moonlight that appears.| |Z|Moonglade|
+F Winterspring
+T Stonespire the Elder |N|In the center of Everlook. (59.9,49.9)| |Z|Winterspring|
+T Brightspear the Elder |N|In the ruins of Kel'Theril. (53.2,56.7)| |Z|Winterspring|
 
 F Azshara
 T Skygleam the Elder |N|In between the legs of the Ravencrest Monument. (64.7,79.3)| |Z|Azshara|
 
-F Winterspring
-T Stonespire the Elder |N|In the center of Everlook. (60,50)| |Z|Winterspring|
-T Brightspear the Elder |N|In the ruins of Kel'Theril. (53.2,56.7)| |Z|Winterspring|
+F Mount Hyjal
+T Evershade the Elder |N|Outside the Nordrassil Inn. (62.5,22.8)| |Z|Mount Hyjal|
+T Windsong the Elder |N|At the Sanctuary of Malorne. (26.6,62)| |Z|Mount Hyjal|
 
-F Felwood |N|Fly to Bloodvenom Post.|
-T Nightwind the Elder |N|North of Jaedenar. (38.3,52.9)| |Z|Felwood|
-
-F Ashenvale |N|Fly to Splinter Tree Post.|
+F Ashenvale
 T Riversong the Elder |N|In the gazebo near the Hippogryph Master. (35.5,48.9)| |Z|Ashenvale|
 
-R Darkshore
-T Starweave the Elder |N|Near Lor'danel. (49.5,18.9)| |Z|Darkshore|
+F Felwood
+T Nightwind the Elder |N|North of Jaedenar. (38.3,52.8)| |Z|Felwood|
 
-b Darnassus |N|Take the boat on the right. (33.17, 40.19)|
-T Bladeswift the Elder |N|In the Cenarion Enclave. (39.1,31.8)| |Z|Darnassus|
+F Darkshore
+T Starweave the Elder |N|Outside Lor'danel. (49.5,18.9)| |Z|Darkshore|
+
+F Darnassus
+T Bladeswift the Elder |N|In the Cenarion Enclave. (39.2,31.8)| |Z|Darnassus|
 T Bladeleaf the Elder |N|Southern edge of Dolanaar. (56.8,53.1)| |Z|Teldrassil|
 
-R Durotar |N|Hearth to Orgrimmar, run out the front door.|
+F Durotar |N|Hearth to Orgrimmar, run out the front door.|
 T Runetotem the Elder |N|South-east edge of Razor Hill. (53.2,43.5)| |Z|Durotar|
 
-R Northern Barrens
+F Northern Barrens
 T Moonwarden the Elder |N|Next to the flight master in the Crossroads. (48.5,59.2)| |Z|Northern Barrens|
 
 F Ratchet
-T Windtotem the Elder |N|In Ratchet, outside of the Engineering building north of the bank. (68.3,70)| |Z|Northern Barrens|
+T Windtotem the Elder |N|In Ratchet, outside of the Engineering building north of the bank. (68.3,69.9)| |Z|Northern Barrens|
 
 F Desolace
-R Maraudon |N|Enter via the Portal to save time if you are over level 30.|
-T Splitrock the Elder |N|Jump into the water by the Princess and Rotgrip. Swim to the east and head up the ramp. Turn right and head up a 2nd ramp. The Elder will be in front of you.|
+F Maraudon |N|Enter the instance via the Portal to Inner Maraudon.|
+T Splitrock the Elder |N|Jump into Rotgrip's pool, swim to the east and head up the ramp. Turn right and head up a second ramp. The Elder will be in front of you.|
 
-R Thunder Bluff |N|Hearth to Orgrimmar, take the zepellin.|
-T Wheathoof the Elder |N|Elder Rise (72.2,23.5). In memoriam, Ezra Chatterton (1998-2008).| |Z|Thunder Bluff|
+F Thunder Bluff |N|Hearth to Orgrimmar, take the zepellin.|
+T Wheathoof the Elder |N|Elder Rise (72.9,23.1). In memoriam, Ezra Chatterton (1998-2008).| |Z|Thunder Bluff|
 
-R Mulgore
+F Mulgore
 T Bloodhoof the Elder |N|North of Bloodhoof Village, next to the bridge (48.5,53.2)| |Z|Mulgore|
 
-R Southern Barrens |N|If you haven't got a flying mount yet, there is a "sort of" path in the cliff west of the Forgotten Oasis.|
+F Southern Barrens
 T High Mountain the Elder |N|Outside of Vendetta Point. (41.5,47.5)| |Z|Southern Barrens|
 
 F Feralas
-T Grimtotem the Elder |N|Lariss Pavilion, northeast of Camp Mojache. (76.7,37.7)| |Z|Feralas|
-T Mistwalker the Elder |N|Inside the Dire Maul arena. (62.6,33)| |Z|Feralas|
+T Grimtotem the Elder |N|Lariss Pavilion, northeast of Camp Mojache. (76.7,37.9)| |Z|Feralas|
+T Mistwalker the Elder |N|Inside the Dire Maul arena. (62.5,31.1)| |Z|Feralas|
 
-R Thousand Needles
+F Thousand Needles
 T Skyseer the Elder |N|Middle of Freewind Post. (46.3,51)| |Z|Thousand Needles|
 T Morningdew the Elder |N|On the speedbarge. (77,75.6)| |Z|Thousand Needles|
 
-R Tanaris
+F Tanaris
 T Dreamseer the Elder |N|Gadgetzan. (51.4,28.8)| |Z|Tanaris|
 
-R Zul'Farrak
-T Wildmane the Elder |N|South-east corner of the room with Gahz'rilla's gong.|
+F Zul'Farrak
+T Wildmane the Elder |N|Inside the instance, in the room with Gahz'rilla's gong.|
 
-R Tanaris
+F Tanaris |N|Exit the instance.|
 T Ragetotem the Elder |N|Valley of the Watchers, west side. (37.2,79)| |Z|Tanaris|
 
-R Un'Goro Crater
-T Thunderhorn the Elder |N|North of the Slithering Scar. (50.3,76.17)| |Z|Un'Goro Crater|
+F Un'Goro Crater
+T Thunderhorn the Elder |N|North of the Slithering Scar. (50.3,76.2)| |Z|Un'Goro Crater|
 
-R Silithus
-T Bladesing the Elder |N|Standing in the Moon Well, Cenarion Hold. (50,35.4)| |Z|Silithus|
+F Uldum |N|Skip ahead to Silithus if you are below Level 83.|
+T Menkhaf the Elder |N|Next to a closed door in Khartut's Tomb. (65.5,18.7)| |Z|Uldum|
+T Sekhemi the Elder |N|In the Ruins of Ammon. (31.6,63)| |Z|Uldum|
+
+F Silithus
+T Bladesing the Elder |N|Standing in the Moon Well, Cenarion Hold. (53,35.4)| |Z|Silithus|
 T Primestone the Elder |N|In the Crystal Vale, near the Ravaged Twilight Camp. (30.8,13.3)| |Z|Silithus|
 
-R Undercity |N|Hearth to Dalaran, take the portal.|
-T Darkcore the Elder |N|In the middle of the throne room inside Undercity, top floor. (67.4,39.1)| |Z|Undercity|
+F Undercity |N|Hearth to Orgrimmar and take the zepellin.|
+T Darkcore the Elder |N|In the middle of the old throne room, surface level. (67.4,39.1)| |Z|Undercity|
 
-R Tirisfal Glades
-T Graveborn the Elder |N|Inside Brill. (61.7,53.7)| |Z|Tirisfal Glades|
+F Tirisfal Glades
+T Graveborn the Elder |N|Inside Brill. (61.8,53.9)| |Z|Tirisfal Glades|
 
 F Silverpine Forest
 T Obsidian the Elder |N|Inside the Sepulcher. (44.9,41.1)| |Z|Silverpine Forest|
@@ -97,110 +102,127 @@ T Obsidian the Elder |N|Inside the Sepulcher. (44.9,41.1)| |Z|Silverpine Forest|
 F The Hinterlands
 T Highpeak the Elder |N|Near the Creeping Ruin. (50,48)| |Z|The Hinterlands|
 
-R Western Plaguelands
-T Meadowrun the Elder |N|North of the road, south of The Weeping Cave. (65.8,47.7)| |Z|Western Plaguelands|
-T Moonstrike the Elder |N|On top of the Scholomance Tower, outside of the instance. (69,74)| |Z|Western Plaguelands|
+F Western Plaguelands
+T Meadowrun the Elder |N|Inside the Weeping Cave. (63.5,36.1)| |Z|Western Plaguelands|
+T Moonstrike the Elder |N|On top of the Scholomance Tower, outside of the instance. (69.2,73.4)| |Z|Western Plaguelands|
 
-R Eastern Plaguelands
+F Eastern Plaguelands
 T Windrun the Elder |N|Inside the Crown Guard Tower, north of Darrowshire. (35.5,68.8)| |Z|Eastern Plaguelands|
 T Snowcrown the Elder |N|Light's Hope Chapel. (75.7,54.5)| |Z|Eastern Plaguelands|
 
-R Stratholme |N|Enter via the Main Entrance.|
+F Stratholme |N|Enter via the Main Entrance.|
 T Farwhisper the Elder |N|North of the Festival Lane mailbox (near The Unforgiven), past the "rat trap". Follow the sign-posts if you get lost.|
 
-R Loch Modan
+F Twilight Highlands
+T Darkfeather the Elder |N|In the Thundermar Ruins. (51.8,33.1)| |Z|Twilight Highlands|
+T Firebeard the Elder |N|In the Dunwald Ruins. (50.9,70.4)| |Z|Twilight Highlands|
+
+F Loch Modan
 T Silvervein the Elder |N|Next to Thelsamar graveyard. (33.3,46.6)| |Z|Loch Modan|
 
-R Dun Morogh
+F Dun Morogh
 T Goldwell the Elder |N|Near the Kharanos inn. (53.9,49.9)| |Z|Dun Morogh|
 
-R City of Ironforge
+F City of Ironforge
 T Bronzebeard the Elder |N|In the pond in the Mystic Ward. (29.2,17) |Z|Ironforge|
 
-R Elwynn Forest
-T Hammershout the Elder |N|Outside Stormwind City, near the gates. (35.5,50)| |Z|Elwynn Forest|
-T Stormbrow the Elder |N|Near the pond. (39.6,63.5)| |Z|Elwynn Forest|
+F Elwynn Forest
+T Hammershout the Elder |N|Outside Stormwind City, near the gates. (34.5,50.2)| |Z|Elwynn Forest|
+T Stormbrow the Elder |N|Near the pond. (39.7,63.6)| |Z|Elwynn Forest|
 
-R Westfall
+F Westfall
 T Skychaser the Elder |N|On top of the tower, north-east of Sentinel Hill. (56.6,47)| |Z|Westfall|
 
 F Northern Stranglethorn
-T Starglade the Elder |N|Near Zul'Gurub, in the right "courtyard" in front of the steps up to instance entrance. (71,34.2)| |Z|Northern Stranglethorn|
+T Starglade the Elder |N|Near Zul'Gurub, in the courtyard in front of the steps up to instance entrance. (71,34.3)| |Z|Northern Stranglethorn|
 
-R The Cape of Stranglethorn |N|Fly to Booty Bay.|
-T Winterhoof the Elder |N|On top of the bank in Booty Bay. (40,72.5)| |Z|The Cape of Stranglethorn|
+F The Cape of Stranglethorn
+T Winterhoof the Elder |N|On top of the bank in Booty Bay. (39.9,72.5)| |Z|The Cape of Stranglethorn|
 
-R The Temple of Atal'Hakkar |N|Fly to the Swamp of Sorrows.|
-T Starsong the Elder |N|At the end of a dead-end corridor inside the instance, to the left of Lord Itharius.|
+F The Temple of Atal'Hakkar |N|Fly to the Swamp of Sorrows.|
+T Starsong the Elder |N|Inside the dungeon, at the end of a short corridor to the left.|
 
 F Blasted Lands
 T Bellowrage the Elder |N|North of the Dark Portal. (54.2,49.5)| |Z|Blasted Lands|
 
-R Burning Steppes
-T Dawnstrider the Elder |N|Up at Flame Crest, that cave with the Libram guy. (53.6,24.4)| |Z|Burning Steppes|
-T Rumblerock the Elder |N|On top of Dreadmaul Rock, north of Morgan's Vigil. (70.1,45.3)| |Z|Burning Steppes|
+F Burning Steppes
+T Dawnstrider the Elder |N|Near Flame Crest. (53.5,24.6)| |Z|Burning Steppes|
+T Rumblerock the Elder |N|On top of Dreadmaul Rock, north of Morgan's Vigil. (70.1,45.4)| |Z|Burning Steppes|
 
 F Searing Gorge
 T Ironband the Elder |N|Southwest corner in Blackchar Cave. (21.3,79)| |Z|Searing Gorge|
 
-R Blackrock Spire
-T Stonefort the Elder |N|On a cliff-edge past the first of the rope bridges in Lower Blackrock Spire.|
-
-R Blackrock Depths
+F Blackrock Depths
 T Morndeep the Elder |N|In the center of the Ring of Law.|
 
+F Blackrock Spire
+T Stonefort the Elder |N|On a cliff edge past the first of the rope bridges in Lower Blackrock Spire.|
+
 H Orgrimmar
-b Borean Tundra
+b Borean Tundra |N|Take the boat at the northernmost pier.|
 T Sardis the Elder |N|Outside Valiance Keep. (59.1,65.6)| |Z|Borean Tundra|
 T Pamuya the Elder |N|In Warsong Hold. (42.9,49.6)| |Z|Borean Tundra|
 T Arp the Elder |N|In the D.E.H.T.A. camp. (57.3,43.7)| |Z|Borean Tundra|
 T Northal the Elder |N|In Transistus Shield. (33.8,34.4)| |Z|Borean Tundra|
 
+R The Nexus
+T Igasho the Elder |N|Before Omorokk the Tree-Shaper.|
+
 F Sholazar Basin |N|Fly to River's Heart from the Transistus Shield. (33.11,34.47)| |Z|Borean Tundra|
 T Sandrene the Elder |N|In Lakeside Landing. (49.8,63)| |Z|Sholazar Basin|
 T Wanikaya the Elder |N|In Rainspeaker Rapids. (63.8,49)| |Z|Sholazar Basin|
 
-F Dragonblight |N|Fly to Star's Rest.|
+R Wintergrasp
+T Bluewolf the Elder |N|Inside the fortress. You can only get in if your faction controls Wintergrasp or if the fortress door has been destroyed. (49,14)| |Z|Wintergrasp|
+
+F Dragonblight
 T Morthie the Elder |N|In Star's Rest. (29.7,55.9)| |Z|Dragonblight|
+
+R Azjol-Nerub
+T Nurgen the Elder |N|Jump down the hole after killing Hadronox. The elder will be on the land, near the water.|
+
+F Dragonblight
 T Skywarden the Elder |N|In Agmar's Hammer. (35.1,48.3)| |Z|Dragonblight|
 T Thoim the Elder |N|In Moa'ki Harbor. (48.8,78.2)| |Z|Dragonblight|
 
-F Grizzly Hills |N|Fly to the Westfall Brigade Encampment|
+F Grizzly Hills
 T Beldak the Elder |N|Westfall Brigade Encampment. (60.6,27.7)| |Z|Grizzly Hills|
 T Whurain the Elder |N|Camp Oneqwah. (64.2,47)| |Z|Grizzly Hills|
 T Lunaro the Elder |N|In the Ruins of Tethys. (80.5,37.6)| |Z|Grizzly Hills|
 
-F Zul'Drak |N|Fly to Zim'Torga.|
+R Gundrak
+T Ohanzee the Elder |N|Near the Drakkari Colossus.|
+
+F Zul'Drak
 T Tauros the Elder |N|In Zim'Torga. (58.9,56)| |Z|Zul'Drak|
+
+R Drak'Tharon Keep
+T Kilias the Elder |N|Before King Dred.|
 
 F The Storm Peaks |N|Fly to K3.|
 T Graymane the Elder |N|In K3. (41.2,84.7)| |Z|The Storm Peaks|
 T Fargal the Elder |N|In Frosthold overlooking the city. (28.9,73.7)| |Z|The Storm Peaks|
 T Stonebeard the Elder |N|In Bouldercrag's Refuge. (31.3,37.6)| |Z|The Storm Peaks|
-T Muraco the Elder |N|Near Camp Taunka'lo. (64.6,51.3)| |Z|The Storm Peaks|
-
-R Wintergrasp
-T Bluewolf the Elder |N|Inside the fortress. You can only get in if your faction controls Wintergrasp or if the fortress door has been destroyed. (49,14)| |Z|Wintergrasp|
-
-R The Nexus
-T Igasho the Elder |N|Before Omorokk the Tree-Shaper.|
 
 R Halls of Stone
 T Yurauk the Elder |N|Before Krystallus.|
 
+F The Storm Peaks
+T Muraco the Elder |N|Near Camp Taunka'lo. (64.6,51.3)| |Z|The Storm Peaks|
+
 R Utgarde Keep
 T Jarten the Elder |N|Before the stairs to Dalronn and Skarvald to the left.|
-
-R Azjol-Nerub
-T Nurgen the Elder |N|Jump down the hole after killing Hadronox. The elder will be on the land, near the water.|
-
-R Drak'Tharon Keep
-T Kilias the Elder |N|Before King Dred.|
 
 R Utgarde Pinnacle
 T Chogan'gada the Elder |N|Under the stairs after Skadi the Ruthless.|
 
-R Gundrak
-T Ohanzee the Elder |N|Near the Drakkari Colossus.|
+H Orgrimmar |N|Or use the portal in Dalaran if you prefer.|
+F Deepholm |N|Use the portal at the Earthshrine.|
+T Stonebrand the Elder |N|Inside the Temple of Earth. (49.7,54.9)| |Z|Deepholm|
+T Deepforge the Elder |N|In Stonehearth. (27.7,69.2)| |Z|Deepholm|
+
+H Orgrimmar |N|Use the portal in the Temple of Earth if your Hearthstone is on cooldown.|
+F Shimmering Expanse |N|Use the portal to Vashj'ir at the Earthshrine.|
+T Moonlance the Elder |N|In the Biel'aran Ridge. (57.2,86.1)| |Z|Shimmering Expanse|
 
 ]] end)
